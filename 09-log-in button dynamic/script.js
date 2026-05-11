bb5db00a1710b2c76feaf8c5307530d1d1553751
Jannatul Ferdous Nabila <samanthanabila47@gmail.com>
2026-05-11 20:15:45 +0600

    Increase decrease font size with button

---
+++ 09-log-in button dynamic/script.js
@@ -0,0 +1,30 @@
+const login = document.getElementById("Login");
+const para = document.getElementById("para");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+const currentSize = window.getComputedStyle(para).fontSize;
+let sizeNumber = parseInt(currentSize);
+
+function increase(value) {
+  sizeNumber = sizeNumber + value;
+
+  para.style.fontSize = sizeNumber + "px";
+  if (sizeNumber >= 30) {
+    plus.setAttribute("disabled", true);
+  } else {
+    plus.removeAttribute("disabled", false);
+  }
+  if (sizeNumber <= 10) {
+    minus.setAttribute("disabled", true);
+  } else {
+    minus.removeAttribute("disabled", false);
+  }
+}
+
+plus.addEventListener("click", () => {
+  increase(2);
+});
+minus.addEventListener("click", () => {
+  increase(-2);
+});
